0a7fe1f6b889a37ff99c0f2a9eb31a6528cf677e
Ray Sylverne <raysylverne@gmail.com>
2023-02-21 10:29:32 -0500

    added reset button to app

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -11,18 +11,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var colorView: UIView!
     
     @IBOutlet weak var blueSwitch: UISwitch!
-    
     @IBOutlet weak var redSwitch: UISwitch!
-    
     @IBOutlet weak var greenSwitch: UISwitch!
     
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
     override func viewDidLoad() {
         
         
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        //colorView.backgroundColor = .black
         updateColor()
     }
     
@@ -32,23 +32,33 @@ class ViewController: UIViewController {
         var blue: CGFloat = 0
         
         if redSwitch.isOn {
-            red = 1
+            red = CGFloat(redSlider.value)
         }
         if greenSwitch.isOn {
-            green = 1
+            green = CGFloat(greenSlider.value)
         }
         if blueSwitch.isOn {
-            blue = 1
+            blue = CGFloat(blueSlider.value)
         }
         let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
         colorView.backgroundColor = color
     }
 
     @IBAction func swithChanged(_ sender: UISwitch) {
-        if sender.isOn {
-            updateColor()
-        } else {
             updateColor()
         }
+    
+    @IBAction func sliderChanged(_ sender: Any) {
+        updateColor()
+    }
+    
+    @IBAction func resetButton(_ sender: Any) {
+        redSwitch.isOn = false
+        greenSwitch.isOn = false
+        blueSwitch.isOn = false
+        redSlider.value = 1
+        greenSlider.value = 1
+        blueSlider.value = 1
+        updateColor()
     }
 }
